22f8e64accf266ac60277f2aa3d9bb19f8333718
urielyonah~ <urielyonah@gmail.com>
2024-06-20 00:28:30 -0600

    archivos properties personalizados

---
+++ src/main/java/com/uriel/curso/springboot/webapp/springboot_web/ValuesConfig.java
@@ -0,0 +1,14 @@
+package com.uriel.curso.springboot.webapp.springboot_web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:values.properties")
+})
+
+public class ValuesConfig {
+
+}
