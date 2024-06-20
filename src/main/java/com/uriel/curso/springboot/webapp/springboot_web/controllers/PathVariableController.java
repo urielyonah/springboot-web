04742dd6dd252581a5e53d93ed6c6ba69336c513
urielyonah~ <urielyonah@gmail.com>
2024-06-20 00:51:13 -0600

    Map SpEL con @Value

--- src/main/java/com/uriel/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/uriel/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -33,6 +33,15 @@ public class PathVariableController {
     @Value("#{ '${config.listOfValues}'.toUpperCase() }")
     private String valueString;
 
+    @Value("#{ ${config.valuesMap} }")
+    private Map<String, Object> valuesMap;
+
+    @Value("#{ ${config.valuesMap}.product }")
+    private String product;
+
+    @Value("#{ ${config.valuesMap}.price }")
+    private Long price;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
 
@@ -65,6 +74,9 @@ public class PathVariableController {
         json.put("listOfValues", listOfValues);
         json.put("valueList", valueList);
         json.put("valueString", valueString);
+        json.put("valuesMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
 
         return json;
     }
